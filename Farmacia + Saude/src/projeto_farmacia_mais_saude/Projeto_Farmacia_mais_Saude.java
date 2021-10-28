f539ad3eb893439b6c157f7dd588ed3772917f27
Ryan Souza <ryan_souza145@hotmail.com>
2021-10-27 22:58:42 -0300

    conexao banco de dados

--- Farmacia + Saude/src/projeto_farmacia_mais_saude/Projeto_Farmacia_mais_Saude.java
+++ Farmacia + Saude/src/projeto_farmacia_mais_saude/Projeto_Farmacia_mais_Saude.java
@@ -1,10 +1,15 @@
 
 package projeto_farmacia_mais_saude;
 
+import Interfaces.tela_Login;
+
+
 public class Projeto_Farmacia_mais_Saude {
 
     public static void main(String[] args) {
         
+        tela_Login Login = new tela_Login();
+        Login.setVisible(true);
         
     }
 }
